0d6f68dcf6684cf1d7ad12ab6a3ade13fe0d0888
chitty8 <juanchitty04@gmail.com>
2022-10-25 19:47:28 -0300

    Initial commit

---
+++ AppCoder/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+
+
+class Curso(models.Model):
+
+    nombre = models.CharField( max_length=50)
+    camada = models.IntegerField()
+
+class Estudiantes(models.Model):
+
+    nombre = models.CharField( max_length=50)
+    apellido = models.CharField( max_length=50)
+    gmail = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField( max_length=50)
+    apellido = models.CharField( max_length=50)
+    gmail = models.EmailField()
+    profesion = models.CharField( max_length=50)
